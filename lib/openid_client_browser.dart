006dd7222238a97c2915d3f8faa1a2a693c7f683
Jamil Velez <jamil.velez@ensabs.eu>
2022-05-09 14:09:56 +0200

    use of universal_html instead of html package

--- lib/openid_client_browser.dart
+++ lib/openid_client_browser.dart
@@ -1,7 +1,8 @@
 import 'openid_client.dart';
-import 'dart:html' hide Credential, Client;
+// import 'dart:html' hide Credential, Client;
 import 'dart:async';
 import 'dart:convert';
+import 'package:universal_html/html.dart' hide Credential, Client;
 export 'openid_client.dart';
 
 class Authenticator {
@@ -12,8 +13,7 @@ class Authenticator {
   Authenticator._(this.flow) : credential = _credentialFromUri(flow);
 
   Authenticator(Client client, {Iterable<String> scopes = const []})
-      : this._(Flow.implicit(client,
-            state: window.localStorage['openid_client:state'])
+      : this._(Flow.implicit(client, state: window.localStorage['openid_client:state'])
           ..scopes.addAll(scopes)
           ..redirectUri = Uri.parse(window.location.href).removeFragment());
 
@@ -27,8 +27,7 @@ class Authenticator {
     _forgetCredentials();
     var c = await credential;
     if (c == null) return;
-    var uri = c.generateLogoutUrl(
-        redirectUri: Uri.parse(window.location.href).removeFragment());
+    var uri = c.generateLogoutUrl(redirectUri: Uri.parse(window.location.href).removeFragment());
     if (uri != null) {
       window.location.href = uri.toString();
     }
@@ -46,17 +45,12 @@ class Authenticator {
     } else {
       var uri = Uri(query: Uri.parse(window.location.href).fragment);
       q = uri.queryParameters;
-      if (q.containsKey('access_token') ||
-          q.containsKey('code') ||
-          q.containsKey('id_token')) {
+      if (q.containsKey('access_token') || q.containsKey('code') || q.containsKey('id_token')) {
         window.localStorage['openid_client:auth'] = json.encode(q);
-        window.location.href =
-            Uri.parse(window.location.href).removeFragment().toString();
+        window.location.href = Uri.parse(window.location.href).removeFragment().toString();
       }
     }
-    if (q!.containsKey('access_token') ||
-        q.containsKey('code') ||
-        q.containsKey('id_token')) {
+    if (q!.containsKey('access_token') || q.containsKey('code') || q.containsKey('id_token')) {
       return await flow.callback(q.cast());
     }
     return null;
